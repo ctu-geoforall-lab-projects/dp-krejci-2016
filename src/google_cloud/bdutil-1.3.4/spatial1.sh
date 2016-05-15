6abee5d88d17ffc394df17e0b7be3919bf11953d
Matej Krejci <matejkrejci@gmail.com>
2016-05-15 19:50:02 +0200

    bigquery

--- src/google_cloud/bdutil-1.3.4/spatial1.sh
+++ src/google_cloud/bdutil-1.3.4/spatial1.sh
@@ -7,7 +7,7 @@ if [[ "${ROLE}" == 'Master' ]]; then
 	do
 		sudo apt-get update
 		if [ $? -eq 0 ];then
-		    sudo apt-get install -y maven  default-jdk git 
+		    sudo apt-get install -y maven  default-jdk git mc
 		    break
 		else
 		    sleep 10
@@ -16,11 +16,13 @@ if [[ "${ROLE}" == 'Master' ]]; then
 
 
 	ins_path=/usr/local/spatial
-	jar_path=/usr/local/spatial/jar
-	sudo mkdir $jar_path
-
+	jar_path=/usr/lib/hive/auxlib/
 	sudo rm -fr  $ins_path
 	sudo mkdir $ins_path
+	sudo mkdir $jar_path
+
+	
+
 	cd $ins_path
 
 	#sudo gsutil -m -o GSUtil:parallel_composite_upload_threshold=150M cp -r gs://init_hadoop/* $ins_path
@@ -39,8 +41,10 @@ if [[ "${ROLE}" == 'Master' ]]; then
 
 	sudo git clone https://github.com/rcongiu/Hive-JSON-Serde.git
 	cd Hive-JSON-Serde  
-	#for hadoop2.x sudo mvn -Phdp23 clean package  https://github.com/rcongiu/Hive-JSON-Serde#compile
-	sudo mvn -Pcdh4 clean package
+	#for hadoop2.x  https://github.com/rcongiu/Hive-JSON-Serde#compile
+	sudo mvn -Phdp23 clean package 
+	#for hadoop 1.x
+	#sudo mvn -Pcdh4 clean package
 	cd $ins_path
 
 	sudo mkdir spatialhadoop
@@ -48,20 +52,24 @@ if [[ "${ROLE}" == 'Master' ]]; then
 	sudo wget http://spatialhadoop.cs.umn.edu/downloads/spatialhadoop-2.3.tar.gz
 	sudo tar xzf spatialhadoop-2.3.tar.gz 
 	#rm $ins_path/spatialhadoop-2.3.tar.gz
-	cd ~
+	cd $jar_path
+	echo pwd
 	pwd
+
 
 
 	sudo cp ${ins_path}/Hive-JSON-Serde/json-serde/target/json-serde-1.3.8-SNAPSHOT-jar-with-dependencies.jar $jar_path
 	sudo cp ${ins_path}/geometry-api-java/target/esri-geometry-api-1.2.1.jar $jar_path
 	sudo cp ${ins_path}/spatial-framework-for-hadoop/hive/target/spatial-sdk-hive-1.1.1-SNAPSHOT.jar $jar_path
-
+	sudo cp ${ins_path}/spatial-framework-for-hadoop/json/target/spatial-sdk-json-1.1.1-SNAPSHOT.jar $jar_path
+	
 	#add path with jars and restart hive
 	#<property>
   	#<name>hive.aux.jars.path</name>
   	#<value>/var/lib/hive</value>
 	#</property>
+	#/etc/hadoop/conf.empty/hadoop-env.sh
+	#export HIVE_AUX_JARS_PATH=/usr/local/spatial/jar
+	#sudo hiveserver2 restart
 
-	sudo hiveserver2 stop
-	sudo hiveserver2 start
 fi
